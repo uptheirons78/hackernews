3ddf288085b8dec15ae02cf9ed8778866411fd75
Mauro Bono <bono.mauro@gmail.com>
2018-03-12 11:08:02 +0100

    update App with new features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -21,23 +21,44 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
     //now the list is part of the component, is internal
-    this.state = { list };
+    //set the initial state
+    this.state = {
+      list: list,
+      searchTerm: "",
+    };
+    /*Binding: in order to make this accessible in your class methods,
+    you have to bind the class methods to this */
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
-
+  //Using ES6 arrow function it is possible to avoid the binding of this in the constructor:
+  //ex: onDismiss = (id) => { do something }
+  //class methods can be autobound automatically without
+  //binding them explicitly by using JavaScript ES6 arrow functions
   onDismiss(id) {
     const updateList = this.state.list.filter(item => item.objectID !== id);
     this.setState({list : updateList});
   }
+  onSearchChange(event) {
+    this.setState({searchTerm: event.target.value});
+  }
 
   render() {
+    //ES6 Object Destructuring
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        {this.state.list.map(item => {
+        <form >
+          <input type="text" name="" id="" onChange={this.onSearchChange} />
+        </form>
+        {this.state.list.filter(isSearched(searchTerm)).map(item => {
           return (
           <div key={item.objectID}>
             <span>
