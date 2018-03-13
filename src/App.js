85d3e7b4165bccff7ff8dea958a43c1a5084c999
Mauro Bono <bono.mauro@gmail.com>
2018-03-13 16:57:16 +0100

    update Search form and add a More button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 //API request URL constants using ES6 Template String
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   //this is a Lifecycle Method in React
@@ -28,7 +28,9 @@ class App extends Component {
     /*Binding: in order to make "this" accessible in your class methods,
     you have to bind the class methods to this */
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
   //Using ES6 arrow function it is possible to avoid the binding of this in the constructor:
@@ -36,16 +38,25 @@ class App extends Component {
   //class methods can be autobound automatically without
   //binding them explicitly by using JavaScript ES6 arrow functions
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result; //collect hits and page from result
+    const oldHits = page !== 0 ? this.state.result.hits : []; //check if there are old hits
+    const updatedHits = [...oldHits, ...hits]; //merge both lists
+    //set the new state
+    this.setState({
+      result: { hits: updatedHits, page}
+    });
   }
   //Here it is possible to Fetch data from API
-  componentDidMount() {
-    const { searchTerm } = this.state;
-
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`) //use default search term to fetch data
+  //Look  page = 0 is an ES6 default parameter
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`) //use default search term to fetch data
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss(id) {
@@ -58,22 +69,31 @@ class App extends Component {
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
   //this is a Lifecycle Method in React
   /* Each time when the state or the props of a component change, the render() method of the
   component is called. */
   render() {
     const { searchTerm, result } = this.state;
-    if (!result) {
-      return null;
-    }
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <div className="iteractions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>
+          <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
             Search
           </Search>
         </div>
-        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
+        {/* Conditional Rendering */}
+        { result && <Table list={result.hits} onDismiss={this.onDismiss} /> }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -84,9 +104,12 @@ class App extends Component {
 //beacause they don’t need access to this.state or this.setState()
 //so ...
 // props as inputs ...JSX as outputs!
-const Search = ({ value, onChange, children }) =>
-    <form>
-      {children} <input type="text" value={value} onChange={onChange} />
+const Search = ({ value, onChange, onSubmit, children }) =>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} />
+      <button type="submit">
+        {children}
+      </button>
     </form>
 
 //Example to add some inline style
@@ -102,9 +125,9 @@ const smallColumn = {
   width: '10%'
 };
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
     <div className="table">
-      {list.filter(isSearched(pattern)).map(item =>
+      {list.map(item =>
         <div key={item.objectID} className="table-row">
           <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
