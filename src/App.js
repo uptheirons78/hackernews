65da0709c93fe1d39ba56e293a7573c440be0df2
Mauro Bono <bono.mauro@gmail.com>
2018-03-13 11:03:13 +0100

    update App fetching data from HackerNews

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+//API request URL constants using ES6 Template String
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
+  //this is a Lifecycle Method in React
+  //is only called when an instance of the component is created and inserted in the DOM.
+  //it is called "Mounting of a Component"
+  // It is called when the component gets initialized.You can set an initial
+  // component state and bind class methods during that lifecycle method.
   constructor(props) {
     super(props);
     //now the list is part of the component, is internal
     //set the initial state
     this.state = {
-      list: list,
-      searchTerm: "",
+      result: null, //empty result initially
+      searchTerm: DEFAULT_QUERY, //default search term
     };
     /*Binding: in order to make "this" accessible in your class methods,
     you have to bind the class methods to this */
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
@@ -41,16 +35,37 @@ class App extends Component {
   //ex: onDismiss = (id) => { do something }
   //class methods can be autobound automatically without
   //binding them explicitly by using JavaScript ES6 arrow functions
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+  //Here it is possible to Fetch data from API
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`) //use default search term to fetch data
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   onDismiss(id) {
-    const updateList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updateList });
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
-
+  //this is a Lifecycle Method in React
+  /* Each time when the state or the props of a component change, the render() method of the
+  component is called. */
   render() {
-    const {searchTerm, list} = this.state;
+    const { searchTerm, result } = this.state;
+    if (!result) {
+      return null;
+    }
     return (
       <div className="page">
         <div className="iteractions">
@@ -58,7 +73,7 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
